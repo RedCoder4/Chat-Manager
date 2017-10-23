f7a60832973020adba3f770ac4318303801f6d9d
RosenDev <33036545+RosenDev@users.noreply.github.com>
2017-10-23 20:56:23 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const botSettings = require("./settings.json");
+const bot = new Discord.Client({ disableEveryone: true });
+const token = botSettings.token;
+const prefix = botSettings.prefix;
+bot.on("message", async message => {
+    let user = message.mentions.users.first();
+    if (!message.author.bot) {
+        let msg = message.content.split(" ");
+        let cont = message.content.slice(prefix.lenght).split(" ");
+        let args = cont.slice(1);
+        let command = msg[0];
+        if (command === `${prefix}fuck`) {
+            channel.cleanContent();
+            message.channel.sendMessage("off");
+        } else if (command === `${prefix}clear`) {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                message.channel.bulkDelete(args);
+            } else {
+                message.channel.sendMessage("you dont ve permission to use the command");
+            }
+
+        } else if (message == "fuck") {
+            message.delete(10);
+        } else if (command === `${prefix}ban`) {
+            if (message.member.hasPermission("BAN_MEMBERS")) {
+                message.guild.ban(user, 2);
+            }
+        } else if (command === `${prefix}unban`) {
+            if (message.member.hasPermission("BAN_MEMBERS")) {
+                message.guild.unban(user);
+            }
+        } else if (command === `${prefix}code`) {
+            message.channel.sendCode("java", "public class Math{ public static void main(String[] args){System.out.println('Hi')}}");
+        }
+    }
+
+
+bot.login(process.env.BOT_TOKEN);
+
